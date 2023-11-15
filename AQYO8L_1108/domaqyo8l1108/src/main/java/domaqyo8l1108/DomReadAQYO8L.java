6a7587b140868698d187a1f5bb2cb7f5aa8f9b6c
MinimalistPeach <stremler.lacika77@gmail.com>
2023-11-15 12:48:51 +0100

    making the query for the eight seminar

--- AQYO8L_1108/domaqyo8l1108/src/main/java/domaqyo8l1108/DomReadAQYO8L.java
+++ AQYO8L_1108/domaqyo8l1108/src/main/java/domaqyo8l1108/DomReadAQYO8L.java
@@ -9,6 +9,7 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -18,69 +19,47 @@ import org.w3c.dom.Node;
 public class DomReadAQYO8L {
 
     public static void main(String[] args) {
-        File xml = new File(
-                "AQYO8L_1108\\domaqyo8l1108\\src\\main\\java\\domaqyo8l1108\\AQYO8L_kurzusfelvetel.xml");
-
         try {
-            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-            SAXParser saxParser = saxParserFactory.newSAXParser();
-            DomHandler handler = new DomHandler();
-            saxParser.parse(xml, handler);
-        } catch (ParserConfigurationException | SAXException | IOException e) {
+            File inputFile = new File("AQYO8L_1108\\\\domaqyo8l1108\\\\src\\\\main\\\\java\\\\domaqyo8l1108\\\\AQYO8L_kurzusfelvetel.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            System.out.println(doc.getDocumentElement().getNodeName());
+
+            NodeList seminarList = doc.getElementsByTagName("kurzus");
+
+            for (int temp = 0; temp < seminarList.getLength(); temp++) {
+                Node seminarNode = seminarList.item(temp);
+
+                if (seminarNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element seminarElement = (Element) seminarNode;
+
+                    String id = seminarElement.getAttribute("id");
+                    String lang = seminarElement.getAttribute("nyelv");
+
+                    System.out.println("<"+seminarElement.getNodeName()+" id=\""+id+"\" nyelv=\""+lang+"\">");
+
+                    System.out.println("<"+seminarElement.getNodeName()+">"+seminarElement.getTextContent()+"</"+seminarElement.getNodeName()+">");
+
+                    System.out.println(
+                            "Kurzusnév: " + seminarElement.getElementsByTagName("kurzusnev").item(0).getTextContent());
+                    System.out.println(
+                            "Kredit: " + seminarElement.getElementsByTagName("kredit").item(0).getTextContent());
+
+                    System.out.println("Hely: " + seminarElement.getElementsByTagName("hely").item(0).getTextContent());
+                    System.out.println(
+                            "Időpont: " + seminarElement.getElementsByTagName("idopont").item(0).getTextContent());
+                    System.out.println(
+                            "Oktató: " + seminarElement.getElementsByTagName("oktato").item(0).getTextContent());
+
+                    if (seminarElement.hasAttribute("jovahagyas"))
+                        System.out.println("Jóváhagyás: " + seminarElement.getAttribute("jovahagyas"));
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-}
-
-class DomHandler extends DefaultHandler {
-
-    private int indent = 0;
-
-    private String formatAttributes(Attributes attributes) {
-        int attrLength = attributes.getLength();
-        if (attrLength == 0)
-            return "";
-
-        StringBuilder sb = new StringBuilder(" ");
-        for (int i = 0; i < attrLength; i++) {
-            sb.append(attributes.getLocalName(i) + "=" + attributes.getValue(i));
-            if (i < attrLength - 1)
-                sb.append(" ");
-        }
-        return sb.toString();
-
-    }
-
-    private void indent() {
-        for (int i = 0; i < indent; i++) {
-            System.out.print("  ");
-        }
-    }
-
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) {
-        if (qName != "kurzus") {
-            indent++;
-            indent();
-        }
-        System.out.print("<" + qName + formatAttributes(attributes) + ">");
-        if (qName == "kurzus" || qName == "AQYO8L_kurzusfelvetel" || qName == "hallgato") {
-            System.out.println();
-        }
-    }
-
-    @Override
-    public void endElement(String uri, String localName, String qName) {
-        System.out.print("</" + qName + ">\n");
-        indent--;
-        indent();
-    }
-
-    @Override
-    public void characters(char[] ch, int start, int length) {
-        String chars = new String(ch, start, length).trim();
-        if (!chars.isEmpty()) {
-            System.out.print(chars);
         }
     }
 }
---
+++ AQYO8L_1115/domqueryaqyo8l/src/main/java/domqueryaqyo8l/DOMQuery.java
@@ -0,0 +1,90 @@
+package AQYO8L_1115.domqueryaqyo8l.src.main.java.domqueryaqyo8l;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class DOMQuery {
+    public static void main(String[] args) {
+        List<String> kurzusList = new ArrayList<>();
+        List<String> hallgatoList = new ArrayList<>();
+        try {
+            File inputFile = new File(
+                    "AQYO8L_1115\\domqueryaqyo8l\\src\\main\\java\\domqueryaqyo8l\\AQYO8L_kurzusfelvetel.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList kurzusNodeList = doc.getElementsByTagName("kurzusnev");
+            System.out.print("Kurzus: [");
+            for (int i = 0; i < kurzusNodeList.getLength(); i++) {
+                Element kurzusElement = (Element) kurzusNodeList.item(i);
+                String kurzus = kurzusElement.getTextContent();
+                kurzusList.add(kurzus);
+                if (i == kurzusNodeList.getLength() - 1) {
+                    System.out.print(kurzus + "]");
+                } else {
+                    System.out.print(kurzus + ", ");
+                }
+            }
+
+            System.out.println("");
+            NodeList hallgatoNodeList = doc.getElementsByTagName("hallgato");
+
+            Document newDoc = dBuilder.newDocument();
+            Element rootElement = newDoc.createElement("hallgatok");
+            newDoc.appendChild(rootElement);
+
+            for (int i = 0; i < hallgatoNodeList.getLength(); i++) {
+                Element hallgatoElement = (Element) hallgatoNodeList.item(i);
+                String hallgato = hallgatoElement.getTextContent();
+                hallgatoList.add(hallgato);
+                Element hallgatoNode = newDoc.createElement("hallgato");
+                hallgatoNode.setAttribute("id", hallgatoElement.getAttribute("id"));
+                rootElement.appendChild(hallgatoNode);
+
+                System.out.println("<hallgato id=\"" + hallgatoElement.getAttribute("id") + "\">");
+
+                Element hnevNode = newDoc.createElement("hnev");
+                hnevNode.appendChild(newDoc.createTextNode(hallgatoElement.getElementsByTagName("hnev").item(0).getTextContent()));
+                hallgatoNode.appendChild(hnevNode);
+
+                System.out.println("<hnev>" + hallgatoElement.getElementsByTagName("hnev").item(0).getTextContent() + "</hnev>");
+
+                Element szulevNode = newDoc.createElement("szulev");
+                szulevNode.appendChild(newDoc.createTextNode(hallgatoElement.getElementsByTagName("szulev").item(0).getTextContent()));
+                hallgatoNode.appendChild(szulevNode);
+
+                System.out.println("<szulev>" + hallgatoElement.getElementsByTagName("szulev").item(0).getTextContent() + "</szulev>");
+
+                Element szakNode = newDoc.createElement("szak");
+                szakNode.setAttribute("evf", hallgatoElement.getElementsByTagName("szak").item(0).getAttributes().getNamedItem("evf").getNodeValue());
+                szakNode.appendChild(newDoc.createTextNode(hallgatoElement.getElementsByTagName("szak").item(0).getTextContent()));
+                hallgatoNode.appendChild(szakNode);
+
+                System.out.println("<szak evf=\"" + hallgatoElement.getElementsByTagName("szak").item(0).getAttributes().getNamedItem("evf").getNodeValue() + "\">" + hallgatoElement.getElementsByTagName("szak").item(0).getTextContent() + "</szak>");
+            }
+
+            System.out.println("</hallgato>");
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(newDoc);
+            StreamResult result = new StreamResult(new File("AQYO8L_1115\\domqueryaqyo8l\\src\\main\\java\\domqueryaqyo8l\\AQYO8L_hallgato.xml"));
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
